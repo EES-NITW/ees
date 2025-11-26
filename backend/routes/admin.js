1a30edb3abc582335554e5b83e9034b06eb3aafc
ishrat2004 <ishratalikhan004@gmail.com>
2025-11-26 13:59:48 +0530

    bootstrapped the frontend folder

--- backend/routes/admin.js
+++ backend/routes/admin.js
@@ -5,4 +5,14 @@ const admin_router=express.Router();
 admin_router.get("/first",(req,res)=>{ 
     res.send("This is admin first route");  
 })
+admin_router.get("/company",(req,res)=>{ 
+    res.send("This is admin first route");  
+})
+admin_router.get("/year",(req,res)=>{ 
+    res.send("This is admin first route");  
+})
+admin_router.get("/students",(req,res)=>{ 
+    res.send("This is admin first route");  
+})
+
 module.exports=admin_router; 
