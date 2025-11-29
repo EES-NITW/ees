ab2455e1d9ebfae7e127528583d976dbbf2180c6
Amit Sharma <amittsharma4567@gmail.com>
2025-11-29 19:27:13 +0530

    Added simple navbar

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -3,30 +3,29 @@ import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 import { BrowserRouter, Route, Routes } from 'react-router-dom'
-import Landing from './pages/Landing'
+import Landing from './Pages/Landing'
 import Events from './Pages/Events'
 import Teams from './Pages/Teams'
 import Placements from './Pages/Placements'
 import Admin from './Pages/Admin'
+import Navbar from "./components/Navbar";
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
-     <div> 
+    <div>
       <BrowserRouter>
-        <Routes> 
-           <Route path="/" element={<Landing/> }/> 
-           <Route path="/events" element={<Events/> }/>
-            <Route path="/teams" element={<Teams/> }/> 
-            <Route path="/placements" element={<Placements/> }/>
-            <Route path="/admin" element={<Admin/> }/> 
+        <Navbar />
+        <Routes>
+          <Route path="/" element={<Landing />} />
+          <Route path="/events" element={<Events />} />
+          <Route path="/teams" element={<Teams />} />
+          <Route path="/placements" element={<Placements />} />
+          <Route path="/admin" element={<Admin />} />
         </Routes>
-
-
       </BrowserRouter>
-
-     </div>
+    </div>
   )
 }
 
