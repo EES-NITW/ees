b1a1a629483e14c017d2ea76bc0886ef36c9c51c
ishrat2004 <ishratalikhan004@gmail.com>
2025-12-03 12:57:10 +0530

    Added tailwincss

--- frontend/src/components/Event_card.jsx
+++ frontend/src/components/Event_card.jsx
@@ -1,7 +1,7 @@
 export function Event_card({ event }) {
   return (
     <div>
-      <h1> Event Card </h1>
+      <h1 className="bg-red-500"> Event Card </h1>
       <p> Event ID : {event.id} </p>
       <p> Event Title : {event.title} </p>
       <p> Description : {event.description} </p>
